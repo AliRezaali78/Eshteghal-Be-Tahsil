a7f252bc0ad0e948b4e26a47879c5990d73b1f37
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-11-28 19:06:27 +0330

    Add validation to automasion register form

--- src/app/automasion/automasion-register/automasion-register.component.ts
+++ src/app/automasion/automasion-register/automasion-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'automasion-register',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./automasion-register.component.css']
 })
 export class AutomasionRegisterComponent implements OnInit {
-
-  constructor() { }
+  form;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      studentCode: ['', [Validators.required, Validators.minLength(14), Validators.maxLength(14), CustomValidators.number]],
+      nationalCode: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), CustomValidators.number]]
+    });
+  }
 
   ngOnInit() {
   }
