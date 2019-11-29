605928282eff829899f37b18845836819724d69e
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-11-29 19:52:58 +0330

    fix authguard

--- src/app/services/guards/auth-guard.service.ts
+++ src/app/services/guards/auth-guard.service.ts
@@ -14,5 +14,6 @@ export class AuthGuard implements CanActivate {
         if (this.authService.isLoggedIn()) return true;
 
         this.router.navigate(['/'], { queryParams: { returnUrl: state.url } })
+        return false;
     }
 }
