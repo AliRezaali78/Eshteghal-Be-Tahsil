0aa2829c22f70d275df8cd73d06e1e935e490d86
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-11-29 23:24:57 +0330

    Style toastr in app handler and change the message

--- src/app/common/errors/app-error-handler.ts
+++ src/app/common/errors/app-error-handler.ts
@@ -1,19 +1,20 @@
-import { ErrorHandler, Inject, NgZone } from '@angular/core';
+import { ErrorHandler, Inject, NgZone, isDevMode } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 export class AppErrorHandler implements ErrorHandler {
     constructor(@Inject(ToastrService) private toastr: ToastrService, private ngZone: NgZone) { }
     handleError(error: any): void {
         this.ngZone.run(() => {
-            this.toastr.error("برای رفع مشکل با پشتیبانی ارتباط برقرار کنید", "خطا", {
-                titleClass: 'text-right',
-                messageClass: 'text-right',
+            this.toastr.error("برای رفع مشکل با پشتیبانی ارتباط برقرار نمایید", "خطا", {
+                titleClass: 'text-right h6',
+                messageClass: 'text-right mat-small',
                 timeOut: 5000,
                 progressBar: true,
                 closeButton: true
             });
         });
-        console.log(error);
+        if (isDevMode())
+            console.log(error);
     }
 
 }
