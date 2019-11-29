6f3b991366ab48dbeb7a569dabc974a106b6b11f
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-11-29 19:34:13 +0330

    Implement return url

--- src/app/automasion/automasion-login/automasion-login.component.ts
+++ src/app/automasion/automasion-login/automasion-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AppError } from 'src/app/common/errors/app.error';
 import { BadRequest } from 'src/app/common/errors/bad-request.error';
 
@@ -14,7 +14,7 @@ export class AutomasionLoginComponent implements OnInit {
   hide = true;
   loading = false;
   invalidLogin: boolean = false;
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -26,11 +26,12 @@ export class AutomasionLoginComponent implements OnInit {
 
   signIn(form: FormGroup) {
     this.loading = true;
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl');
     this.authService.login(form.value).subscribe(result => {
       this.loading = false;
       if (result) {
         this.invalidLogin = false;
-        this.router.navigate(['/main']);
+        this.router.navigate([returnUrl || '/main']);
       } else this.invalidLogin = true;
     }, (error: AppError) => {
       this.loading = false;
