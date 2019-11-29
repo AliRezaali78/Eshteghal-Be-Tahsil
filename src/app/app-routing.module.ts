950c3306437edc04d38df45ffb9bbec4b2801a2c
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-11-29 17:43:34 +0330

    Add login and main page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AutomasionComponent } from './automasion/automasion.component';
+import { MainComponent } from './main/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AutomasionComponent },
+  { path: "/main", component: MainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
