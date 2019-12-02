4a2d43d13a057d104c8e634b4fcebb7f00dde04e
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-12-03 01:22:35 +0330

    Add extra fields to registration form

--- src/app/automasion/automasion-register/automasion-register.component.ts
+++ src/app/automasion/automasion-register/automasion-register.component.ts
@@ -12,31 +12,42 @@ import { Event } from '@angular/router';
     fadeInOutCustom
   ]
 })
-export class AutomasionRegisterComponent implements OnInit {
+export class AutomasionRegisterComponent {
   form: FormGroup;
   sk = "6LduRcQUAAAAAJW6-ot5RJBEt-5IfsSevCFxfbev";
   loading = false;
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
+  majors = [
+    { id: 1, value: "کامپیوتر - نرم افزار کامپیوتر" },
+    { id: 2, value: "الکترونیک" },
+    { id: 3, value: "برق" },
+  ]
+  constructor(private fb: FormBuilder) {
+    this.form = this.createForm();
+  }
+
+  createForm() {
+    return this.fb.group({
       name: ['', Validators.required],
       lastname: ['', Validators.required],
       father: ['', Validators.required],
       studentCode: ['', [Validators.required, Validators.minLength(14), Validators.maxLength(14), CustomValidators.number]],
       nationalCode: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), CustomValidators.number]],
+      major: ['', Validators.required],
+      level: ['', Validators.required],
+      type: ['', Validators.required],
       reCaptcha: ['', Validators.required]
     });
   }
 
-  ngOnInit() {
-  }
-
-  get showCaptcha() {
+  get showCaptcha(): boolean {
     return (this.form.get('name').valid &&
       this.form.get('lastname').valid &&
       this.form.get('studentCode').valid &&
       this.form.get('father').valid &&
-      this.form.get('nationalCode')).valid ? true : false;
-
+      this.form.get('nationalCode')).valid &&
+      this.form.get('major').valid &&
+      this.form.get('level').valid &&
+      this.form.get('type').valid ? true : false;
   }
 
 
