384f5b484045ec7fe182da94de80df4c0d1d1ec9
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-12-03 04:36:46 +0330

    Implement ngOnDestroy in profile

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { fadeIn } from '../../common/animations/fade.animation';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { SidenavService } from '../../services/sidenav/sidenav.service';
 import { MatTabGroup } from '@angular/material';
@@ -12,20 +12,23 @@ import { delay } from 'rxjs/operators';
   styleUrls: ['./profile.component.scss'],
   animations: [fadeIn]
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
   @ViewChild('tabs') tabs: MatTabGroup;
   subs: Subscription;
   constructor(private auth: AuthService, private sidenav: SidenavService) {
   }
 
   ngOnInit() {
-    this.sidenav.switch.pipe(delay(500)).subscribe(() => {
+    this.subs = this.sidenav.switch.pipe(delay(500)).subscribe(() => {
       this.tabs.realignInkBar();
     })
   }
 
-
   get user() {
     return this.auth.currentUser;
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
