5119a987c06ce6444c92367c05771aa8f8f72d9d
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-12-11 23:40:40 +0330

    Add table to requests component

--- src/app/main/student/requests/requests.component.ts
+++ src/app/main/student/requests/requests.component.ts
@@ -1,12 +1,20 @@
+import { fadeIn } from './../../../common/animations/fade.animation';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.scss']
+  styleUrls: ['./requests.component.scss'],
+  animations: [fadeIn]
 })
 export class RequestsComponent implements OnInit {
 
+  displayedColumns: string[] = ['id', 'option', 'to', 'status', 'actions'];
+  requests = [
+    { id: 1, option: 'مرکز', to: 'مرکز تست', status: '1' },
+    { id: 2, option: 'مرکز', to: 'مرکز تست', status: '2' },
+    { id: 3, option: 'سازمان', to: 'سازمان تست', status: '3' }
+  ];
   constructor() { }
 
   ngOnInit() {
