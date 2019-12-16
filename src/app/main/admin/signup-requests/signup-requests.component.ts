8d3a8523e53444b48b00e125d126772ab10611b8
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-12-16 23:49:16 +0330

    add signup requests component

---
+++ src/app/main/admin/signup-requests/signup-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'signup-requests',
+  templateUrl: './signup-requests.component.html',
+  styleUrls: ['./signup-requests.component.scss']
+})
+export class SignupRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
