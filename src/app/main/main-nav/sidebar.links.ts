54b1e53ebcf80304aecb9d453e583959a5f2bdb3
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-12-28 03:37:50 +0330

    change sidebar texts

--- src/app/main/main-nav/sidebar.links.ts
+++ src/app/main/main-nav/sidebar.links.ts
@@ -1,10 +1,10 @@
 export const USER_LINKS = [
     { text: 'درخواست جدید', routerLink: 'new-request', icon: 'fal fa-file-plus ml-2' },
-    { text: 'درخواست های جاری', routerLink: 'requests', icon: 'fal fa-table ml-2' },
+    { text: 'درخواست های من', routerLink: 'requests', icon: 'fal fa-table ml-2' },
 ]
 
 export const ADMIN_LINKS = [
-    { text: 'دانشجو ها', routerLink: 'students', icon: 'fal fa-users ml-2' },
+    { text: 'دانشجویان', routerLink: 'students', icon: 'fal fa-users ml-2' },
     { text: 'درخواست ها', routerLink: 'review-requests', icon: 'fal fa-table ml-2' },
     { text: 'ثبت اطلاعت دانشجو', routerLink: 'student-form', icon: 'fal fa-user-edit ml-2' },
     { text: 'درخواست های ثبت نام', routerLink: 'signup-requests', icon: 'fal fa-user-plus ml-2' },
